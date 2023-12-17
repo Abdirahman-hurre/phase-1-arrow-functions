da6ef6b0c9c56e932895f9beb9e87d10ea9e6de3
Abdirahman <abdirahman.abdi01@student.moringaschool.com>
2023-12-17 18:16:56 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = function (num1, num2) {
+    if (num1 !== undefined && num2 !== undefined) {
+      return num1 / num2;
+    } else {
+      return 2000 / 100;
+    }
+}
+const square= num => {
+    return num * num;
+}
+
+const add = (num3, num4 ) => {
+    return num3 + num4;
+} 
